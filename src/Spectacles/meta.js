3a7b41aac6bbdd4fa7f58fab498827b9c34b3215
Quantix <9021453+quantix-dev@users.noreply.github.com>
2022-06-04 17:20:54 +0000

    chore: increment version

--- src/Spectacles/meta.js
+++ src/Spectacles/meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        %name%
-// @version     1.1.0
+// @version     1.1.1
 // @description A reddit userscript to effortlessly remove all spoilers in posts, and make it crystal clear.
 // @namespace   %namespace%
 // @homepageURL %homepage%
